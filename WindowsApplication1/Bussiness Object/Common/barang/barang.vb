d602f4b02fc4cf4749ef111e38e10a74802fe47f
wawaboyz <wawaboyz@ymail.com>
2014-11-27 22:42:20 +0700

    tes

--- WindowsApplication1/Bussiness Object/Common/barang/barang.vb
+++ WindowsApplication1/Bussiness Object/Common/barang/barang.vb
@@ -11,6 +11,26 @@ Namespace BussinessObject.Common
         Private hargaperkiraan As Decimal
         Private hargajual As Decimal
         Private isdelete As Boolean
+        Private barangpaket As NHibernate.Collection.PersistentSet
+        Private pembelianbarang As NHibernate.Collection.PersistentSet
+        Private stokadjustmentbarang As NHibernate.Collection.PersistentSet
+
+        Public Sub New()
+
+        End Sub
+
+        Sub New(ByVal idbar As Int32, ByVal namabar As String, ByVal stokbar As Int32, ByVal inventar As Boolean, ByVal hargaper As Decimal, ByVal hargaj As Decimal, ByVal isdel As Boolean, ByVal barangp As NHibernate.Collection.PersistentSet, ByVal pembar As NHibernate.Collection.PersistentSet, ByVal stokad As NHibernate.Collection.PersistentSet)
+            idbarang = idbar
+            namabarang = namabar
+            stokbarang = stokbar
+            inventar = inventar
+            hargaperkiraan = hargaper
+            hargajual = hargaj
+            isdelete = isdel
+            barangpaket = barangp
+            pembelianbarang = pembar
+            stokadjustmentbarang = stokad
+        End Sub
 
 
         Public Overridable Property eidbarang() As Int32
@@ -43,12 +63,12 @@ Namespace BussinessObject.Common
             End Set
         End Property
 
-        Public Overridable Property einventaris() As Int32
+        Public Overridable Property einventaris() As Boolean
             Get
                 Return inventaris
             End Get
 
-            Set(ByVal value As Int32)
+            Set(ByVal value As Boolean)
                 inventaris = value
             End Set
         End Property
@@ -82,6 +102,34 @@ Namespace BussinessObject.Common
                 isdelete = value
             End Set
         End Property
+
+        Public Overridable Property ebarangpaket() As NHibernate.Collection.Generic.PersistentGenericSet(Of BarangPaket)
+            Get
+                Return barangpaket
+            End Get
+            Set(value As NHibernate.Collection.Generic.PersistentGenericSet(Of BarangPaket))
+                barangpaket = value
+            End Set
+        End Property
+
+        Public Overridable Property epembelianbarang() As NHibernate.Collection.Generic.PersistentGenericSet(Of PembelianBarang)
+            Get
+                Return pembelianbarang
+            End Get
+            Set(value As NHibernate.Collection.Generic.PersistentGenericSet(Of PembelianBarang))
+                pembelianbarang = value
+            End Set
+        End Property
+
+        Public Overridable Property estokadjustmentbarang() As NHibernate.Collection.Generic.PersistentGenericSet(Of StokAdjustmentBarang)
+            Get
+                Return stokadjustmentbarang
+            End Get
+            Set(value As NHibernate.Collection.Generic.PersistentGenericSet(Of StokAdjustmentBarang))
+                stokadjustmentbarang = value
+            End Set
+        End Property
+
 
     End Class
 End Namespace
