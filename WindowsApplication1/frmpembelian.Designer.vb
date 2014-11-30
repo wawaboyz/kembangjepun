f3e419bf1354eef6cf1d34c72f9133a8c78ee130
wawaboyz <wawaboyz@ymail.com>
2014-11-30 22:24:46 +0700

    30

--- WindowsApplication1/frmpembelian.Designer.vb
+++ WindowsApplication1/frmpembelian.Designer.vb
@@ -30,24 +30,26 @@ Partial Class frmpembelian
         Me.lblsupplier = New System.Windows.Forms.Label()
         Me.lblidfaktur = New System.Windows.Forms.Label()
         Me.grouplistbarang = New System.Windows.Forms.GroupBox()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.dglistbarang = New System.Windows.Forms.DataGridView()
         Me.txtcari = New System.Windows.Forms.TextBox()
         Me.lblcari = New System.Windows.Forms.Label()
         Me.groupbarang = New System.Windows.Forms.GroupBox()
+        Me.btntambahbarang = New System.Windows.Forms.Button()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.TextBox5 = New System.Windows.Forms.TextBox()
-        Me.TextBox4 = New System.Windows.Forms.TextBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txthargabeli = New System.Windows.Forms.TextBox()
+        Me.txtjum = New System.Windows.Forms.TextBox()
+        Me.txtstokbarang = New System.Windows.Forms.TextBox()
+        Me.txtnmbarang = New System.Windows.Forms.TextBox()
+        Me.txtidbarang = New System.Windows.Forms.TextBox()
         Me.lblhargabeli = New System.Windows.Forms.Label()
         Me.lbljumlahbeli = New System.Windows.Forms.Label()
         Me.lblstokbarang = New System.Windows.Forms.Label()
         Me.lblnmbarang = New System.Windows.Forms.Label()
         Me.lblidbarang = New System.Windows.Forms.Label()
         Me.grpdatatransaksi = New System.Windows.Forms.GroupBox()
-        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.btndelete = New System.Windows.Forms.Button()
+        Me.dgtransaksi = New System.Windows.Forms.DataGridView()
         Me.chktax = New System.Windows.Forms.CheckBox()
         Me.lbltax = New System.Windows.Forms.Label()
         Me.lblgrandtotal = New System.Windows.Forms.Label()
@@ -55,18 +57,12 @@ Partial Class frmpembelian
         Me.TextBox7 = New System.Windows.Forms.TextBox()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.dgidbarang = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.dgnamabarang = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.dgjumlah = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.dgharga = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.dgsubtotal = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.btntambahbarang = New System.Windows.Forms.Button()
         Me.grpdatapembelian.SuspendLayout()
         Me.grouplistbarang.SuspendLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dglistbarang, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.groupbarang.SuspendLayout()
         Me.grpdatatransaksi.SuspendLayout()
-        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dgtransaksi, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'grpdatapembelian
@@ -79,7 +75,7 @@ Partial Class frmpembelian
         Me.grpdatapembelian.Controls.Add(Me.lblidfaktur)
         Me.grpdatapembelian.Location = New System.Drawing.Point(12, 13)
         Me.grpdatapembelian.Name = "grpdatapembelian"
-        Me.grpdatapembelian.Size = New System.Drawing.Size(467, 149)
+        Me.grpdatapembelian.Size = New System.Drawing.Size(467, 139)
         Me.grpdatapembelian.TabIndex = 0
         Me.grpdatapembelian.TabStop = False
         Me.grpdatapembelian.Text = "DATA PEMBELIAN"
@@ -135,7 +131,7 @@ Partial Class frmpembelian
         '
         'grouplistbarang
         '
-        Me.grouplistbarang.Controls.Add(Me.DataGridView1)
+        Me.grouplistbarang.Controls.Add(Me.dglistbarang)
         Me.grouplistbarang.Controls.Add(Me.txtcari)
         Me.grouplistbarang.Controls.Add(Me.lblcari)
         Me.grouplistbarang.Location = New System.Drawing.Point(495, 13)
@@ -145,13 +141,15 @@ Partial Class frmpembelian
         Me.grouplistbarang.TabStop = False
         Me.grouplistbarang.Text = "LIST BARANG"
         '
-        'DataGridView1
+        'dglistbarang
         '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(18, 50)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(312, 269)
-        Me.DataGridView1.TabIndex = 2
+        Me.dglistbarang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dglistbarang.Location = New System.Drawing.Point(18, 50)
+        Me.dglistbarang.Name = "dglistbarang"
+        Me.dglistbarang.ReadOnly = True
+        Me.dglistbarang.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dglistbarang.Size = New System.Drawing.Size(312, 269)
+        Me.dglistbarang.TabIndex = 2
         '
         'txtcari
         '
@@ -174,22 +172,31 @@ Partial Class frmpembelian
         Me.groupbarang.Controls.Add(Me.btntambahbarang)
         Me.groupbarang.Controls.Add(Me.Label2)
         Me.groupbarang.Controls.Add(Me.Label1)
-        Me.groupbarang.Controls.Add(Me.TextBox5)
-        Me.groupbarang.Controls.Add(Me.TextBox4)
-        Me.groupbarang.Controls.Add(Me.TextBox3)
-        Me.groupbarang.Controls.Add(Me.TextBox2)
-        Me.groupbarang.Controls.Add(Me.TextBox1)
+        Me.groupbarang.Controls.Add(Me.txthargabeli)
+        Me.groupbarang.Controls.Add(Me.txtjum)
+        Me.groupbarang.Controls.Add(Me.txtstokbarang)
+        Me.groupbarang.Controls.Add(Me.txtnmbarang)
+        Me.groupbarang.Controls.Add(Me.txtidbarang)
         Me.groupbarang.Controls.Add(Me.lblhargabeli)
         Me.groupbarang.Controls.Add(Me.lbljumlahbeli)
         Me.groupbarang.Controls.Add(Me.lblstokbarang)
         Me.groupbarang.Controls.Add(Me.lblnmbarang)
         Me.groupbarang.Controls.Add(Me.lblidbarang)
-        Me.groupbarang.Location = New System.Drawing.Point(20, 168)
+        Me.groupbarang.Location = New System.Drawing.Point(20, 158)
         Me.groupbarang.Name = "groupbarang"
-        Me.groupbarang.Size = New System.Drawing.Size(459, 184)
+        Me.groupbarang.Size = New System.Drawing.Size(459, 194)
         Me.groupbarang.TabIndex = 2
         Me.groupbarang.TabStop = False
         Me.groupbarang.Text = "BARANG"
+        '
+        'btntambahbarang
+        '
+        Me.btntambahbarang.Location = New System.Drawing.Point(152, 152)
+        Me.btntambahbarang.Name = "btntambahbarang"
+        Me.btntambahbarang.Size = New System.Drawing.Size(141, 23)
+        Me.btntambahbarang.TabIndex = 13
+        Me.btntambahbarang.Text = "TAMBAH BARANG"
+        Me.btntambahbarang.UseVisualStyleBackColor = True
         '
         'Label2
         '
@@ -209,48 +216,48 @@ Partial Class frmpembelian
         Me.Label1.TabIndex = 11
         Me.Label1.Text = "Rp"
         '
-        'TextBox5
+        'txthargabeli
         '
-        Me.TextBox5.Location = New System.Drawing.Point(131, 123)
-        Me.TextBox5.Name = "TextBox5"
-        Me.TextBox5.Size = New System.Drawing.Size(108, 20)
-        Me.TextBox5.TabIndex = 10
-        Me.TextBox5.Text = "0"
-        Me.TextBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        Me.txthargabeli.Location = New System.Drawing.Point(131, 123)
+        Me.txthargabeli.Name = "txthargabeli"
+        Me.txthargabeli.Size = New System.Drawing.Size(108, 20)
+        Me.txthargabeli.TabIndex = 10
+        Me.txthargabeli.Text = "0"
+        Me.txthargabeli.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         '
-        'TextBox4
+        'txtjum
         '
-        Me.TextBox4.Location = New System.Drawing.Point(111, 99)
-        Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(59, 20)
-        Me.TextBox4.TabIndex = 9
-        Me.TextBox4.Text = "0"
-        Me.TextBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        Me.txtjum.Location = New System.Drawing.Point(111, 99)
+        Me.txtjum.Name = "txtjum"
+        Me.txtjum.Size = New System.Drawing.Size(59, 20)
+        Me.txtjum.TabIndex = 9
+        Me.txtjum.Text = "0"
+        Me.txtjum.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         '
-        'TextBox3
+        'txtstokbarang
         '
-        Me.TextBox3.Enabled = False
-        Me.TextBox3.Location = New System.Drawing.Point(110, 74)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(60, 20)
-        Me.TextBox3.TabIndex = 8
-        Me.TextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        Me.txtstokbarang.Location = New System.Drawing.Point(110, 74)
+        Me.txtstokbarang.Name = "txtstokbarang"
+        Me.txtstokbarang.ReadOnly = True
+        Me.txtstokbarang.Size = New System.Drawing.Size(60, 20)
+        Me.txtstokbarang.TabIndex = 8
+        Me.txtstokbarang.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         '
-        'TextBox2
+        'txtnmbarang
         '
-        Me.TextBox2.Enabled = False
-        Me.TextBox2.Location = New System.Drawing.Point(110, 48)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(181, 20)
-        Me.TextBox2.TabIndex = 7
+        Me.txtnmbarang.Location = New System.Drawing.Point(110, 48)
+        Me.txtnmbarang.Name = "txtnmbarang"
+        Me.txtnmbarang.ReadOnly = True
+        Me.txtnmbarang.Size = New System.Drawing.Size(181, 20)
+        Me.txtnmbarang.TabIndex = 7
         '
-        'TextBox1
+        'txtidbarang
         '
-        Me.TextBox1.Enabled = False
-        Me.TextBox1.Location = New System.Drawing.Point(111, 23)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 6
+        Me.txtidbarang.Location = New System.Drawing.Point(111, 23)
+        Me.txtidbarang.Name = "txtidbarang"
+        Me.txtidbarang.ReadOnly = True
+        Me.txtidbarang.Size = New System.Drawing.Size(100, 20)
+        Me.txtidbarang.TabIndex = 6
         '
         'lblhargabeli
         '
@@ -299,7 +306,8 @@ Partial Class frmpembelian
         '
         'grpdatatransaksi
         '
-        Me.grpdatatransaksi.Controls.Add(Me.DataGridView2)
+        Me.grpdatatransaksi.Controls.Add(Me.btndelete)
+        Me.grpdatatransaksi.Controls.Add(Me.dgtransaksi)
         Me.grpdatatransaksi.Location = New System.Drawing.Point(20, 370)
         Me.grpdatatransaksi.Name = "grpdatatransaksi"
         Me.grpdatatransaksi.Size = New System.Drawing.Size(816, 196)
@@ -307,14 +315,25 @@ Partial Class frmpembelian
         Me.grpdatatransaksi.TabStop = False
         Me.grpdatatransaksi.Text = "DATA TRANSAKSI"
         '
-        'DataGridView2
+        'btndelete
         '
-        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView2.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.dgidbarang, Me.dgnamabarang, Me.dgjumlah, Me.dgharga, Me.dgsubtotal})
-        Me.DataGridView2.Location = New System.Drawing.Point(9, 19)
-        Me.DataGridView2.Name = "DataGridView2"
-        Me.DataGridView2.Size = New System.Drawing.Size(785, 161)
-        Me.DataGridView2.TabIndex = 0
+        Me.btndelete.Location = New System.Drawing.Point(705, 165)
+        Me.btndelete.Name = "btndelete"
+        Me.btndelete.Size = New System.Drawing.Size(75, 23)
+        Me.btndelete.TabIndex = 11
+        Me.btndelete.Text = "DELETE"
+        Me.btndelete.UseVisualStyleBackColor = True
+        '
+        'dgtransaksi
+        '
+        Me.dgtransaksi.AllowUserToAddRows = False
+        Me.dgtransaksi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgtransaksi.Location = New System.Drawing.Point(9, 19)
+        Me.dgtransaksi.Name = "dgtransaksi"
+        Me.dgtransaksi.ReadOnly = True
+        Me.dgtransaksi.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgtransaksi.Size = New System.Drawing.Size(785, 140)
+        Me.dgtransaksi.TabIndex = 0
         '
         'chktax
         '
@@ -388,54 +407,12 @@ Partial Class frmpembelian
         Me.Label4.TabIndex = 9
         Me.Label4.Text = "Rp"
         '
-        'dgidbarang
-        '
-        Me.dgidbarang.HeaderText = "ID BARANG"
-        Me.dgidbarang.Name = "dgidbarang"
-        Me.dgidbarang.ReadOnly = True
-        '
-        'dgnamabarang
-        '
-        Me.dgnamabarang.HeaderText = "NAMA BARANG"
-        Me.dgnamabarang.Name = "dgnamabarang"
-        Me.dgnamabarang.ReadOnly = True
-        Me.dgnamabarang.Width = 200
-        '
-        'dgjumlah
-        '
-        Me.dgjumlah.HeaderText = "JUMLAH "
-        Me.dgjumlah.Name = "dgjumlah"
-        Me.dgjumlah.ReadOnly = True
-        Me.dgjumlah.Width = 80
-        '
-        'dgharga
-        '
-        Me.dgharga.HeaderText = "HARGA BARANG"
-        Me.dgharga.Name = "dgharga"
-        Me.dgharga.ReadOnly = True
-        Me.dgharga.Width = 150
-        '
-        'dgsubtotal
-        '
-        Me.dgsubtotal.HeaderText = "SUBTOTAL"
-        Me.dgsubtotal.Name = "dgsubtotal"
-        Me.dgsubtotal.ReadOnly = True
-        Me.dgsubtotal.Width = 200
-        '
-        'btntambahbarang
-        '
-        Me.btntambahbarang.Location = New System.Drawing.Point(152, 152)
-        Me.btntambahbarang.Name = "btntambahbarang"
-        Me.btntambahbarang.Size = New System.Drawing.Size(141, 23)
-        Me.btntambahbarang.TabIndex = 13
-        Me.btntambahbarang.Text = "TAMBAH BARANG"
-        Me.btntambahbarang.UseVisualStyleBackColor = True
-        '
         'frmpembelian
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(849, 663)
+        Me.AutoSize = True
+        Me.ClientSize = New System.Drawing.Size(953, 664)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.TextBox7)
@@ -453,11 +430,11 @@ Partial Class frmpembelian
         Me.grpdatapembelian.PerformLayout()
         Me.grouplistbarang.ResumeLayout(False)
         Me.grouplistbarang.PerformLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dglistbarang, System.ComponentModel.ISupportInitialize).EndInit()
         Me.groupbarang.ResumeLayout(False)
         Me.groupbarang.PerformLayout()
         Me.grpdatatransaksi.ResumeLayout(False)
-        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgtransaksi, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -470,7 +447,7 @@ Partial Class frmpembelian
     Friend WithEvents txtidbeli As System.Windows.Forms.TextBox
     Friend WithEvents txtsupplier As System.Windows.Forms.TextBox
     Friend WithEvents grouplistbarang As System.Windows.Forms.GroupBox
-    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents dglistbarang As System.Windows.Forms.DataGridView
     Friend WithEvents txtcari As System.Windows.Forms.TextBox
     Friend WithEvents lblcari As System.Windows.Forms.Label
     Friend WithEvents groupbarang As System.Windows.Forms.GroupBox
@@ -481,13 +458,12 @@ Partial Class frmpembelian
     Friend WithEvents lblidbarang As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents txthargabeli As System.Windows.Forms.TextBox
+    Friend WithEvents txtjum As System.Windows.Forms.TextBox
+    Friend WithEvents txtstokbarang As System.Windows.Forms.TextBox
+    Friend WithEvents txtnmbarang As System.Windows.Forms.TextBox
+    Friend WithEvents txtidbarang As System.Windows.Forms.TextBox
     Friend WithEvents grpdatatransaksi As System.Windows.Forms.GroupBox
-    Friend WithEvents DataGridView2 As System.Windows.Forms.DataGridView
     Friend WithEvents chktax As System.Windows.Forms.CheckBox
     Friend WithEvents lbltax As System.Windows.Forms.Label
     Friend WithEvents lblgrandtotal As System.Windows.Forms.Label
@@ -495,10 +471,7 @@ Partial Class frmpembelian
     Friend WithEvents TextBox7 As System.Windows.Forms.TextBox
     Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents Label4 As System.Windows.Forms.Label
-    Friend WithEvents dgidbarang As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents dgnamabarang As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents dgjumlah As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents dgharga As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents dgsubtotal As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents btntambahbarang As System.Windows.Forms.Button
+    Friend WithEvents btndelete As System.Windows.Forms.Button
+    Friend WithEvents dgtransaksi As System.Windows.Forms.DataGridView
 End Class
